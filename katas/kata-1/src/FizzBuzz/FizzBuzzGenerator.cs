b6535aee74c898c00c956dab2641678435b44ac1
Santiago Rodriguez <santinacional123.sr@gmail.com>
2025-12-05 14:38:23 -0500

    🟢

--- katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
+++ katas/kata-1/src/FizzBuzz/FizzBuzzGenerator.cs
@@ -4,7 +4,24 @@ namespace FizzBuzz
     {
         public static List<string> Generate(int count)
         {
-          throw new Exception("FizzBuzzGenerator generation not implemented");
+            var result = new List<string>();
+            for (var number = 1; number <= count; number++)
+            {
+                if (IsMultipleOf(number, 15))
+                {
+                    result.Add("FizzBuzz");
+                    break;
+                }
+
+                if (IsMultipleOf(number, 3))
+                    result.Add("Fizz");
+                else if (IsMultipleOf(number, 5))
+                    result.Add("Buzz");
+                else
+                    result.Add(number.ToString());
+            }
+
+            return result;
         }
 
         private static bool IsMultipleOf(int number, int multiple) => number % multiple == 0;
